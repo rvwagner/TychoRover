2289644dc5a84e0fbe0b71aa89cfda19d03c0d39
Robert Wagner <rvwagner@asu.edu>
2018-02-01 16:49:46 -0700

    Created script and message infrastructure for sensor data, including fake data producer for the mini-Tycho rover.

---
+++ scripts/coallate_display_panel.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+import rospy
+from tycho.msg import WheelStatus, RoverPosition, DisplayPanel
+from math import atan, atan2, sqrt, pi
+import operator
+
+# Builds up packets to send to the display panel
+# Sends combined packets at a fixed rate, based on the most recent data
+# from the various sensors
+# TODO: Send immediate packets for error and warning states
+# Author: Robert Wagner
+
+update_delay = 0.1 # seconds between updates
+
+
+class CommandToAngles:
+    def __init__(self):
+        rospy.Subscriber("tycho/wheel_status", WheelStatus, self.wheel_callback)
+        rospy.Subscriber("tycho/rover_position_orientation", RoverPosition, self.pose_callback)
+        
+        # Direct message parts
+        self.speed               = 0.0
+        self.front_left_angle    = 0.0
+        self.front_right_angle   = 0.0
+        self.back_left_angle     = 0.0
+        self.back_right_angle    = 0.0
+        self.roll                = 0.0
+        self.pitch               = 0.0
+        self.heading             = 0.0
+        
+        # Helper variables
+        self.last_pose_time      = rospy.Time.now()
+        self.last_wheel_times    = [self.last_pose_time,self.last_pose_time,self.last_pose_time,self.last_pose_time]
+        self.max_amp_hours       = 100.0
+        self.remaining_amp_hours = self.max_amp_hours # TODO: Handle battery percent better
+        self.amp_list            = [0.,0.,0.,0.]
+        self.drive_temp_list     = [0.,0.,0.,0.]
+        
+        self.pub = rospy.Publisher('tycho/display_values', DisplayPanel)
+    #
+    
+    
+    
+    # Extract data from WheelStatus messages
+    def wheel_callback(self, data):
+        """
+        
+        """
+        
+        dt = data.header.stamp - self.last_wheel_timesdata.wheel_id-1] # TODO: Is this in seconds? It should be
+        self.last_wheel_timesdata.wheel_id-1] = data.header.stamp
+        
+        if data.wheel_id == 1:   # Front-left
+            self.front_left_angle = data.steering_angle
+        elif data.wheel_id == 2: # Front-right
+            self.front_right_angle = data.steering_angle
+        elif data.wheel_id == 3: # Back-left
+            self.back_left_angle = data.steering_angle
+        elif data.wheel_id == 4: # Back-right
+            self.back_right_angle = data.steering_angle
+        #
+        
+        # TODO: Handle remaining amp-hours better
+        self.amp_list[data.wheel_id-1] = data.drive_amps + data.steering_amps
+        self.remaining_amp_hours      -= dt*self.amp_list[data.wheel_id-1]/3600.0
+        
+        # TODO: Maybe allow for non-drive motors being hottest?
+        self.drive_temp_list[data.wheel_id-1] = data.drive_temp )
+        
+        # TODO: Derive speed from avg RPM and wheel diameter?
+        # TODO: Should we display distance driven?
+        # TODO: Should we display crows-flight distance to start?
+        
+        # TODO: Should flag states be determined here, or on the Arduino?
+        
+        # TODO: Immediate send for error conditions, e.g. T>90
+        if max(data.drive_temp, data.steering_temp)>= 90:
+            self.publishMessage()
+    #
+    
+    
+    def pose_callback(self, data):
+        """
+        
+        """
+        
+        dt = data.header.stamp - self.last_pose_time
+        self.last_pose_time = data.header.stamp
+        
+        self.speed   = data.speed # TODO: Derive speed from avg RPM and wheel diameter?
+        self.roll    = data.roll
+        self.pitch   = data.pitch
+        self.heading = data.heading
+    #
+    
+    
+    def publishMessage(self):
+        m = DisplayPanel()
+        m.header.stamp = rospy.Time.now()
+        m.speed             = self.speed
+        m.front_left_angle  = self.front_left_angle
+        m.front_right_angle = self.front_right_angle
+        m.back_left_angle   = self.back_left_angle
+        m.back_right_angle  = self.back_right_angle
+        m.total_amps        = sum(self.amp_list)
+        m.battery_pct       = self.remaining_amp_hours/self.max_amp_hours
+        m.hottest_motor_id, m.max_drive_temp = max(enumerate(self.drive_temp_list), key=operator.itemgetter(1))
+        m.hottest_motor_id  += 1
+        m.roll              = self.roll
+        m.pitch             = self.pitch
+        m.heading           = self.heading
+        
+        self.pub.publish(m)
+    #
+    
+
+# Intializes everything
+def start():
+    interpreter = CommandToAngles() # subscribes to joystick inputs on topic "joy"
+    
+    # starts the node
+    rospy.init_node('Model2Raw')
+    while not rospy.core.is_shutdown():
+        rospy.rostime.wallsleep(update_delay)
+        interpreter.publishMessage()
+#
+
+if __name__ == '__main__':
+    start()
+    
---
+++ scripts/fake_wheel_status.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+import rospy
+from tycho.msg import WheelAnglesSpeeds, WheelStatus
+
+# Create fake WheelStatus messages based on commands sent to the wheels
+# Author: Robert Wagner
+
+
+class MakeFakeWheelStatus:
+    def __init__(self):
+        rospy.Subscriber("tycho/low_level_motor_values", RoverDriveCommand, self.lowlevel_callback)
+        
+        self.old_drive_temperatures = [40,40,40,40]
+        self.old_steer_temperatures = [40,40,40,40]
+        self.old_cntrl_temperatures = [40,40,40,40]
+        
+        self.timestamp = rospy.Time.now()
+        
+        self.pub = rospy.Publisher('tycho/wheel_status', WheelStatus)
+    #
+    
+    
+    
+    # Reads high-level commands (e.g. strafing at 1 m/s),
+    # sets speed and r/p/y values
+    def lowlevel_callback(self, data):
+        """
+        Reads low-level commands (e.g. front left wheel at x degrees),
+        and creates fake "WheelStatus" messages
+        """
+        
+        # Get time delta
+        dt = data.header.stamp - self.timestamp # TODO: Is this in seconds? It should be
+        self.timestamp = data.header.stamp
+        
+        wheel1Status = WheelStatus()
+        wheel1Status.wheel_id        = 1
+        wheel1Status.drive_rpm       = data.front_left_speed * 175.0 # Assumes speed is scaled -1 to 1
+        wheel1Status.steering_angle  = data.front_left_angle
+        wheel1Status.drive_amps      = data.front_left_speed * 150
+        wheel1Status.steering_amps   = 1
+        wheel1Status.drive_spin_count = wheel1Status.drive_rpm / 60.0 * dt
+        wheel1Status.drive_temp      = old_drive_temperatures[0] + (wheel1Status.drive_amps - 30)**2*dt/100
+        wheel1Status.steering_temp   = 40
+        wheel1Status.controller_temp = old_cntrl_temperatures[0] + (wheel1Status.drive_amps - 30)**2*dt/500
+        
+        
+        wheel2Status = WheelStatus()
+        wheel2Status.wheel_id        = 2
+        wheel2Status.drive_rpm       = data.front_right_speed * 175.0
+        wheel2Status.steering_angle  = data.front_right_angle
+        wheel2Status.drive_amps      = data.front_right_speed * 150
+        wheel2Status.steering_amps   = 1
+        wheel2Status.drive_spin_count = wheel2Status.drive_rpm / 60.0 * dt
+        wheel2Status.drive_temp      = old_drive_temperatures[1] + (wheel2Status.drive_amps - 30)**2*dt/100
+        wheel2Status.steering_temp   = 40
+        wheel2Status.controller_temp = old_cntrl_temperatures[1] + (wheel2Status.drive_amps - 30)**2*dt/500
+        
+        
+        wheel3Status = WheelStatus()
+        wheel3Status.wheel_id        = 3
+        wheel3Status.drive_rpm       = data.back_left_speed * 175.0
+        wheel3Status.steering_angle  = data.back_left_angle
+        wheel3Status.drive_amps      = data.back_left_speed * 150
+        wheel3Status.steering_amps   = 1
+        wheel3Status.drive_spin_count = wheel3Status.drive_rpm / 60.0 * dt
+        wheel3Status.drive_temp      = old_drive_temperatures[2] + (wheel3Status.drive_amps - 30)**2*dt/100
+        wheel3Status.steering_temp   = 40
+        wheel3Status.controller_temp = old_cntrl_temperatures[2] + (wheel3Status.drive_amps - 30)**2*dt/500
+        
+        
+        wheel4Status = WheelStatus()
+        wheel4Status.wheel_id        = 4
+        wheel4Status.drive_rpm       = data.back_right_speed * 175.0
+        wheel4Status.steering_angle  = data.back_right_angle
+        wheel4Status.drive_amps      = data.back_right_speed * 150
+        wheel4Status.steering_amps   = 1
+        wheel4Status.drive_spin_count = wheel4Status.drive_rpm / 60.0 * dt
+        wheel4Status.drive_temp      = old_drive_temperatures[3] + (wheel4Status.drive_amps - 30)**2*dt/100
+        wheel4Status.steering_temp   = 40
+        wheel4Status.controller_temp = old_cntrl_temperatures[3] + (wheel4Status.drive_amps - 30)**2*dt/500
+        
+        
+        old_drive_temperatures = [wheel1Status.drive_temp, wheel2Status.drive_temp, wheel3Status.drive_temp, wheel4Status.drive_temp]
+        old_steer_temperatures = [wheel1Status.steering_temp, wheel2Status.steering_temp, wheel3Status.steering_temp, wheel4Status.steering_temp]
+        old_cntrl_temperatures = [wheel1Status.controller_temp, wheel2Status.controller_temp, wheel3Status.controller_temp, wheel4Status.controller_temp]
+        
+        
+        self.pub.publish(wheel1Status)
+        self.pub.publish(wheel2Status)
+        self.pub.publish(wheel3Status)
+        self.pub.publish(wheel4Status)
+    #
+    
+
+# Intializes everything
+def start():
+    interpreter = MakeFakeWheelStatus() # subscribes to low-level commands
+    
+    # starts the node
+    rospy.init_node('Raw2FakeStatus')
+    rospy.spin()
+#
+
+if __name__ == '__main__':
+    start()
+    
